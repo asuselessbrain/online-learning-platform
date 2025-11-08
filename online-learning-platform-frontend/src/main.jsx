2f2cdedd76896763ad1ce72c70ef5bbb8d8fc0bb
asuselessbrain <arfan18@cse.pstu.ac.bd>
2025-11-08 22:45:22 +0600

    Add: tailwind, react hot toast, react router setup

--- online-learning-platform-frontend/src/main.jsx
+++ online-learning-platform-frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router'
+import { routes } from './routes/Routes.jsx'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
+    <ToastContainer />
   </StrictMode>,
 )
---
+++ online-learning-platform-frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,10 @@
+
+const Login = () => {
+    return (
+        <div>
+           <h2 className="text-red-500">Login</h2>
+        </div>
+    );
+};
+
+export default Login;
